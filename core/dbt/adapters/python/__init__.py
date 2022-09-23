504056be68264ab40b1c7cc68dd842d08116a31b
Matteo Ferrando <matteo.ferrando2@gmail.com>
2022-09-23 17:45:30 -0400

    Add base PythonAdapter and PythonConnectionManager classes

---
+++ core/dbt/adapters/python/__init__.py
@@ -0,0 +1,3 @@
+# these are all just exports, #noqa them so flake8 will be happy
+from dbt.adapters.python.connections import PythonConnectionManager  # noqa
+from dbt.adapters.python.impl import PythonAdapter  # noqa
